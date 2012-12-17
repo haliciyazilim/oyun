43cc37ba37016bf68b18875b303f921b6b92961b
Alperen Kavun <alperen@halici.com.tr>
2012-12-17 14:29:57 +0200

    updateStripes methods are rearranged

--- OkParcalari/OkParcalari/Stopwatch.m
+++ OkParcalari/OkParcalari/Stopwatch.m
@@ -77,20 +77,32 @@
 - (void) initializeTimerSprites {
     CGSize size = [[CCDirector sharedDirector] winSize];
     
-    CCSprite *timerNum1 = [CCSprite spriteWithFile:@"timer_num_0.png"];
-    timerNum1.position = ccp(size.width * 0.817, size.height * 0.5445);
+    _sprite1File = @"timer_num_0.png";
+    _sprite2File = @"timer_num_0.png";
+    _sprite3File = @"timer_num_0.png";
+    _sprite4File = @"timer_num_0.png";
+    _dotFile = @"timer_num_nokta.png";
+    NSLog(@"%@",_sprite1File);
     
-    CCSprite *timerNum2 = [CCSprite spriteWithFile:@"timer_num_0.png"];
+    CCSprite *timerNum1 = [CCSprite spriteWithFile:_sprite1File];
+    timerNum1.position = ccp(size.width * 0.817, size.height * 0.5445);
+    timerNum1.tag = 11;
+    
+    CCSprite *timerNum2 = [CCSprite spriteWithFile:_sprite2File];
     timerNum2.position = ccp(size.width * 0.847, size.height * 0.5445);
+    timerNum2.tag = 22;
 
-    CCSprite *timerNum3 = [CCSprite spriteWithFile:@"timer_num_0.png"];
+    CCSprite *timerNum3 = [CCSprite spriteWithFile:_sprite3File];
     timerNum3.position = ccp(size.width * 0.889, size.height * 0.5445);
+    timerNum3.tag = 33;
 
-    CCSprite *timerNum4 = [CCSprite spriteWithFile:@"timer_num_0.png"];
+    CCSprite *timerNum4 = [CCSprite spriteWithFile:_sprite4File];
     timerNum4.position = ccp(size.width * 0.919, size.height * 0.5445);
+    timerNum4.tag = 44;
 
-    CCSprite *timerDot = [CCSprite spriteWithFile:@"timer_num_nokta.png"];
+    CCSprite *timerDot = [CCSprite spriteWithFile:_dotFile];
     timerDot.position = ccp(size.width * 0.867, size.height * 0.5445);
+    timerDot.tag = 55;
     
     [self addChild:timerNum1];
     [self addChild:timerNum2];
@@ -101,12 +113,11 @@
 }
 
 - (void) updateTimerSprites {
-    [self removeAllChildrenWithCleanup:NO];
+//    [self removeAllChildrenWithCleanup:NO];
     
     CGSize size = [[CCDirector sharedDirector] winSize];
     
     NSString *fileName1,*fileName2,*fileName3,*fileName4;
-    
     
     if (_minutes < 10) {
         fileName1 = @"timer_num_0.png";
@@ -125,27 +136,38 @@
         fileName3 = [NSString stringWithFormat:@"timer_num_%lld.png",_seconds/10];
         fileName4 = [NSString stringWithFormat:@"timer_num_%lld.png",_seconds%10];
     }
-    
-    CCSprite *timerNum1 = [CCSprite spriteWithFile:fileName1];
-    timerNum1.position = ccp(size.width * 0.818, size.height * 0.5445);
-    
-    CCSprite *timerNum2 = [CCSprite spriteWithFile:fileName2];
-    timerNum2.position = ccp(size.width * 0.848, size.height * 0.5445);
-    
-    CCSprite *timerNum3 = [CCSprite spriteWithFile:fileName3];
-    timerNum3.position = ccp(size.width * 0.890, size.height * 0.5445);
-    
-    CCSprite *timerNum4 = [CCSprite spriteWithFile:fileName4];
-    timerNum4.position = ccp(size.width * 0.920, size.height * 0.5445);
-    
-    CCSprite *timerDot = [CCSprite spriteWithFile:@"timer_num_nokta.png"];
-    timerDot.position = ccp(size.width * 0.868, size.height * 0.5445);
-    
-    [self addChild:timerNum1];
-    [self addChild:timerNum2];
-    [self addChild:timerDot];
-    [self addChild:timerNum3];
-    [self addChild:timerNum4];
+    if(![_sprite1File  isEqualToString:fileName1]){
+        [self removeChildByTag:11 cleanup:YES];
+        CCSprite *timerNum1 = [CCSprite spriteWithFile:fileName1];
+        timerNum1.position = ccp(size.width * 0.818, size.height * 0.5445);
+        timerNum1.tag = 11;
+        [self addChild:timerNum1];
+        _sprite1File = fileName1;
+    }
+    if(![_sprite2File isEqualToString:fileName2]){
+        [self removeChildByTag:22 cleanup:YES];
+        CCSprite *timerNum2 = [CCSprite spriteWithFile:fileName2];
+        timerNum2.position = ccp(size.width * 0.848, size.height * 0.5445);
+        timerNum2.tag = 22;
+        [self addChild:timerNum2];
+        _sprite2File = fileName2;
+    }
+    if(![_sprite3File isEqualToString:fileName3]){
+        [self removeChildByTag:33 cleanup:YES];
+        CCSprite *timerNum3 = [CCSprite spriteWithFile:fileName3];
+        timerNum3.position = ccp(size.width * 0.890, size.height * 0.5445);
+        timerNum3.tag = 33;
+        [self addChild:timerNum3];
+        _sprite3File = fileName3;
+    }
+    if(![_sprite4File isEqualToString:fileName4]){
+        [self removeChildByTag:44 cleanup:YES];
+        CCSprite *timerNum4 = [CCSprite spriteWithFile:fileName4];
+        timerNum4.position = ccp(size.width * 0.920, size.height * 0.5445);
+        timerNum4.tag = 44;
+        [self addChild:timerNum4];
+        _sprite4File = fileName4;
+    }
 }
 
 @end
