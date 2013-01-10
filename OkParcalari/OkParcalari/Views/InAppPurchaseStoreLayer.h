4902d28a254877a1b1512eaa876966ee7b49beb0
Alperen Kavun <alperen@halici.com.tr>
2013-01-10 14:48:42 +0200

    comm

---
+++ OkParcalari/OkParcalari/Views/InAppPurchaseStoreLayer.h
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseStoreLayer.h
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 10.01.2013.
+//
+//
+
+#import "CCLayer.h"
+
+@interface InAppPurchaseStoreLayer : CCLayer
+
+@end
---
+++ OkParcalari/OkParcalari/Views/InAppPurchaseStoreLayer.m
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseStoreLayer.m
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 10.01.2013.
+//
+//
+
+#import "InAppPurchaseStoreLayer.h"
+
+@implementation InAppPurchaseStoreLayer
+
+@end
