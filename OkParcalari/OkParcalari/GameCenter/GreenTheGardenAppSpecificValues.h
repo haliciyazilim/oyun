bf6355a9af1573774fc93bb9051348b84f3b498b
Alperen Kavun <alperen@halici.com.tr>
2013-01-20 14:54:23 +0200

    Added appSpecificValues class for game center achievements

---
+++ OkParcalari/OkParcalari/GameCenter/GreenTheGardenAppSpecificValues.h
@@ -0,0 +1,13 @@
+//
+//  GreenTheGardenAppSpecificValues.h
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 18.01.2013.
+//
+//
+
+// Leaderboard IDs (if any)
+
+// Achievements IDs
+#define kAchievementBabySteps @"com.halici.GreenTheGarden.babySteps"
+#define kAchievementFirstSolution @"com.halici.GreenTheGarden.firstSolution"
---
+++ OkParcalari/OkParcalari/GameCenter/GreenTheGardenAppSpecificValues.m
@@ -0,0 +1,13 @@
+//
+//  GreenTheGardenAppSpecificValues.m
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 18.01.2013.
+//
+//
+
+#import "GreenTheGardenAppSpecificValues.h"
+
+@implementation GreenTheGardenAppSpecificValues
+
+@end
