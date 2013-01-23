fcb53ac6a8f18ea6ec1bd47a1aec166141a22aa3
Alperen Kavun <alperen@halici.com.tr>
2013-01-23 15:35:33 +0200

    product descriptions are added

--- OkParcalari/OkParcalari/InAppPurchase/GreenTheGardenIAPHelper.m
+++ OkParcalari/OkParcalari/InAppPurchase/GreenTheGardenIAPHelper.m
@@ -51,10 +51,31 @@
     [unlockImage setFrame:CGRectMake(355.0, 260.0, 153.0, 178.0)];
     
     NSString *header = [[self.callerLayer.products objectAtIndex:0] localizedTitle];
-    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectMake(768.0, 260.0, 165.0, 40.0)];
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectMake(522.0, 260.0, 160.0, 40.0)];
+    [headerLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:22.0]];
     [headerLabel setBackgroundColor:[UIColor clearColor]];
-    [headerLabel setTextAlignment:NSTextAlignmentCenter];
+    [headerLabel setTextAlignment:NSTextAlignmentLeft];
     [headerLabel setText:header];
+    
+    NSString *description = [[self.callerLayer.products objectAtIndex:0] localizedDescription];
+    UILabel *descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(522.0, 300.0, 160.0, 100.0)];
+    [descriptionLabel setFont:[UIFont fontWithName:@"Helvetica" size:20.0]];
+    [descriptionLabel setBackgroundColor:[UIColor clearColor]];
+    [descriptionLabel setTextAlignment:NSTextAlignmentLeft];
+    [descriptionLabel setNumberOfLines:3];
+    [descriptionLabel setText:description];
+    
+    NSNumberFormatter *priceFormatter = [[NSNumberFormatter alloc] init];
+    [priceFormatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
+    [priceFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    
+    NSString *priceStr = [priceFormatter stringFromNumber:[[self.callerLayer.products objectAtIndex:0] price]];
+    
+    UILabel *priceLabel = [[UILabel alloc] initWithFrame:CGRectMake(522.0, 400.0, 160.0, 40.0)];
+    [priceLabel setFont:[UIFont fontWithName:@"Helvetica" size:20.0]];
+    [priceLabel setBackgroundColor:[UIColor clearColor]];
+    [priceLabel setTextAlignment:NSTextAlignmentLeft];
+    [priceLabel setText:priceStr];
     
     [storeView addSubview:backgroundView];
     [storeView addSubview:backView];
@@ -63,6 +84,8 @@
     [storeView addSubview:restoreButton];
     [storeView addSubview:unlockImage];
     [storeView addSubview:headerLabel];
+    [storeView addSubview:descriptionLabel];
+    [storeView addSubview:priceLabel];
     
     return storeView;
 }
