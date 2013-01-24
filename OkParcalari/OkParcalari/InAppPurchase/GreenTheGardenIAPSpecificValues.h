1a2813486414cd1c2498be580774b35ad2f5e8c3
Alperen Kavun <alperen@halici.com.tr>
2013-01-24 15:49:25 +0200

    little bug fixed

---
+++ OkParcalari/OkParcalari/InAppPurchase/GreenTheGardenIAPSpecificValues.h
@@ -0,0 +1,12 @@
+//
+//  GreenTheGardenIAPSpecificValues.h
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 24.01.2013.
+//
+
+// product identifiers
+#define iProUpgradeKey @"com.halici.GreenTheGarden.gameUnlock"
+
+// product keys
+#define iProUpgradeSecret @"o94k<lE[xxZ"
---
+++ OkParcalari/OkParcalari/InAppPurchase/GreenTheGardenIAPSpecificValues.m
@@ -0,0 +1,13 @@
+//
+//  GreenTheGardenIAPSpecificValues.m
+//  GreenTheGarden
+//
+//  Created by Alperen Kavun on 24.01.2013.
+//
+//
+
+#import "GreenTheGardenIAPSpecificValues.h"
+
+@implementation GreenTheGardenIAPSpecificValues
+
+@end
