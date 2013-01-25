4d9a0679a2528ac75ff3e42a8db0d1103d54d9f5
edebimce <abdullah@halici.com.tr>
2013-01-25 17:00:12 +0200

    Achievement constans added

--- OkParcalari/OkParcalari/GameCenter/GreenTheGardenGCSpecificValues.h
+++ OkParcalari/OkParcalari/GameCenter/GreenTheGardenGCSpecificValues.h
@@ -11,6 +11,31 @@
 // Achievements IDs
 #define kAchievementWarmingUp @"com.halici.GreenTheGarden.achievements.WarmingUp"
 #define kAchievementPathToStardom @"com.halici.GreenTheGarden.achievements.PathToStardom"
+#define kAchievementBabySteps @"com.halici.GreenTheGarden.achievements.BabySteps"
+#define kAchievementFastMindQuickHands @"com.halici.GreenTheGarden.achievements.FastMindQuickHands"
+#define kAchievementFreeMapCompletionist @"com.halici.GreenTheGarden.achievements.FreeMapCompletionist"
+#define kAchievementFreeMapsPerfectionist @"com.halici.GreenTheGarden.achievements.FreeMapsPerfectionist"
+#define kAchievementStubborn @"com.halici.GreenTheGarden.achievements.Stubborn"
+
+#define kAchievementBipolar @"com.halici.GreenTheGarden.achievements.Bipolar"
+#define kAchievementNothingToDoHere @"com.halici.GreenTheGarden.achievements.NothingToDoHere"
+#define kAchievementTrustMeINowWhatImDoing @"com.halici.GreenTheGarden.achievements.TrustMeINowWhatImDoing"
+#define kAchievementBeAPro @"com.halici.GreenTheGarden.achievements.BeAPro"
+
+#define kAchievementEasyMapsCompletionist @"com.halici.GreenTheGarden.achievements.EasyMapsCompletionist"
+#define kAchievementEasyMapsPerfectionist @"com.halici.GreenTheGarden.achievements.EasyMapsPerfectionist"
+#define kAchievementNormalMapsCompletionist @"com.halici.GreenTheGarden.achievements.NormalMapsCompletionist"
+#define kAchievementNormalMapsPerfectionist @"com.halici.GreenTheGarden.achievements.NormalMapsPerfectionist"
+
+#define kAchievementHardMapsCompletionist @"com.halici.GreenTheGarden.achievements.HardMapsCompletionist"
+#define kAchievementHardMapsPerfectionist @"com.halici.GreenTheGarden.achievements.HardMapsPerfectionist"
+#define kAchievementInsaneMapsCompletionist @"com.halici.GreenTheGarden.achievements.InsaneMapsCompletionist"
+#define kAchievementInsaneMapsPerfectionist @"com.halici.GreenTheGarden.achievements.InsaneMapsPerfectionist"
+
+#define kAchievementTotalMapsCompletionist @"com.halici.GreenTheGarden.achievements.TotalMapsCompletionist"
+#define kAchievementTotalMapsPerfectionist @"com.halici.GreenTheGarden.achievements.TotalMapsPerfectionist"
+
+#define kAchievementTheRestaurant @"com.halici.GreenTheGarden.achievements.TheRestaurant"
 
 // GameCemter NOtifications
 #define kAuthenticationChangedNotification @"AuthenticedChanged"
