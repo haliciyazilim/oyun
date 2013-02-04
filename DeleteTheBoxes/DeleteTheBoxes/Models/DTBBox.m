ee449ae7eb94cbf79426602297fd602d5e84fc92
Alperen Kavun <alperen@halici.com.tr>
2013-02-04 17:59:52 +0200

    class methods decleration will be completed

---
+++ DeleteTheBoxes/DeleteTheBoxes/Models/DTBBox.m
@@ -0,0 +1,70 @@
+//
+//  DTBBox.m
+//  DeleteTheBoxes
+//
+//  Created by Alperen Kavun on 04.02.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "DTBBox.h"
+
+static int classOrder = 0;
+
+@implementation DTBBox
+{
+    // hold a private variable which points to line
+    // for adding and removing line easily
+    
+}
+
++ (id) BoxWithFrame:(CGRect)frame andTitle:(NSString *)title {
+    return [[DTBBox alloc] initWithFrame:frame andTitle:title];
+}
+
+- (id) initWithFrame:(CGRect)frame andTitle:(NSString *)title {
+    if (self = [self initWithFrame:frame]) {
+        self.title = title;
+        self.isDeleted = NO;
+        self.order = classOrder;
+        classOrder++;
+    }
+    return self;
+}
+- (id) initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+- (void) deleteBox {
+    self.isDeleted = YES;
+    [self animateBoxToOutside];
+}
+- (void) resetBox {
+    self.isDeleted = NO;
+    [self animateBoxToInside];
+}
+- (void) animateBoxToOutside {
+    [self drawLineToOriginalPosition];
+}
+- (void) animateBoxToInside {
+    [self removeLine];
+}
+- (void) drawLineToOriginalPosition {
+    
+}
+- (void) removeLine {
+    
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
