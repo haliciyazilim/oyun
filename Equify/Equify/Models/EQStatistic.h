8533ad2935cddc86d50fb2759db1101c1f906b96
Alperen Kavun <alperen@halici.com.tr>
2013-03-13 09:29:50 +0200

    EQ: Added EQStatistic class

---
+++ Equify/Equify/Models/EQStatistic.h
@@ -0,0 +1,22 @@
+//
+//  EQStatistic.h
+//  Equify
+//
+//  Created by Alperen Kavun on 12.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface EQStatistic : NSManagedObject
+
+@property int minTime;
+@property int maxTime;
+@property int totalSolvedQuestion;
+@property int totalSkippedQuestion;
+@property int allTimeAverage;
+
++ (EQStatistic *)initializeStatistics;
++ (
+
+@end
---
+++ Equify/Equify/Models/EQStatistic.m
@@ -0,0 +1,19 @@
+//
+//  EQStatistic.m
+//  Equify
+//
+//  Created by Alperen Kavun on 12.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQStatistic.h"
+
+@implementation EQStatistic
+
+@dynamic minTime;
+@dynamic maxTime;
+@dynamic totalSolvedQuestion;
+@dynamic totalSkippedQuestion;
+@dynamic allTimeAverage;
+
+@end
