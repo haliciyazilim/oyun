04ab7717561eaaf615e38a007c437dfc76b60b0f
Alperen Kavun <alperen@halici.com.tr>
2013-03-13 09:29:35 +0200

    EQ: Added EQScore class

---
+++ Equify/Equify/Models/EQScore.h
@@ -0,0 +1,13 @@
+//
+//  EQScore.h
+//  Equify
+//
+//  Created by Alperen Kavun on 12.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface EQScore : NSManagedObject
+
+@end
---
+++ Equify/Equify/Models/EQScore.m
@@ -0,0 +1,13 @@
+//
+//  EQScore.m
+//  Equify
+//
+//  Created by Alperen Kavun on 12.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQScore.h"
+
+@implementation EQScore
+
+@end
