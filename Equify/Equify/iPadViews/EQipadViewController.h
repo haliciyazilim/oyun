3e0181d02655c4dba7c0092947c09e4178be8cb2
edebimce <abdullah@halici.com.tr>
2013-03-14 14:48:38 +0200

    EQ - ipad storyboard and classes

---
+++ Equify/Equify/iPadViews/EQipadViewController.h
@@ -0,0 +1,13 @@
+//
+//  EQipadViewController.h
+//  Equify
+//
+//  Created by Abdullah Karacabey on 14.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQViewController.h"
+
+@interface EQipadViewController : EQViewController
+
+@end
---
+++ Equify/Equify/iPadViews/EQipadViewController.m
@@ -0,0 +1,39 @@
+//
+//  EQipadViewController.m
+//  Equify
+//
+//  Created by Abdullah Karacabey on 14.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQipadViewController.h"
+
+@interface EQipadViewController ()
+
+@end
+
+@implementation EQipadViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    buttonsViewHeight=204;
+    buttonsViewWidth=404;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
