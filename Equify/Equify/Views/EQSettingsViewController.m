5358527019cdeb83b38fe37decda517166defd75
edebimce <abdullah@halici.com.tr>
2013-03-18 14:36:25 +0200

    EQ - settingsbutton and strings

--- Equify/Equify/Views/EQSettingsViewController.m
+++ Equify/Equify/Views/EQSettingsViewController.m
@@ -12,7 +12,10 @@
 
 @end
 
-@implementation EQSettingsViewController
+@implementation EQSettingsViewController{
+    CGFloat buttonWidth;
+    CGFloat buttonHeight;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -30,26 +33,43 @@
     
     CGFloat winWidth = [[UIScreen mainScreen] bounds].size.height;
     CGFloat winHeight = [[UIScreen mainScreen] bounds].size.width;
-
+    buttonWidth=175;
+    buttonHeight=40;
     UIView *seperator1 = [[UIView alloc] initWithFrame:CGRectMake((winWidth-175)/2, (winHeight-40)/2-55, 175, 3.0)];
     [seperator1 setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"double_line.png"]]];
 
+    /*
     UIButton * btnReset=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2-50, 175, 40) title:@"reset stats"];
+    [btnReset setTitleColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:1.0] forState:UIControlStateNormal];
+//    [btnReset setTitle:title forState:UIControlStateNormal];
+    [btnReset setTitleShadowColor:[UIColor colorWithWhite:1.0 alpha:0.7] forState:UIControlStateNormal];
+    */
+    
+    
+    UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [closeButton setBackgroundImage:[UIImage imageNamed:@"close_btn.png"] forState:UIControlStateNormal];
+    [closeButton setBackgroundImage:[UIImage imageNamed:@"close_btn_pressed.png"] forState:UIControlStateHighlighted];
+    [closeButton addTarget:self action:@selector(closeStats) forControlEvents:UIControlEventTouchUpInside];
+    closeButton.frame = CGRectMake(winWidth-45.0, 5.0, 35.0, 35.0);
+    
+    
+    UIButton * btnReset=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2-50, 175, 40) title:NSLocalizedString(@"RESET", nil)];
+
     
     UIView *seperator2 = [[UIView alloc] initWithFrame:CGRectMake((winWidth-175)/2, (winHeight-40)/2-5, 175, 3.0)];
     [seperator2 setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"double_line.png"]]];
 
-    UIButton * btnAbout=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2, 175, 40) title:@"about us"];
+    UIButton * btnAbout=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2, 175, 40) title:NSLocalizedString(@"ABOUT", nil)];
 
     UIView *seperator3 = [[UIView alloc] initWithFrame:CGRectMake((winWidth-175)/2, (winHeight-40)/2+45, 175, 3.0)];
     [seperator3 setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"double_line.png"]]];
 
-    UIButton * btnMoreGames=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2+50, 175, 40) title:@"more games"];
+    UIButton * btnMoreGames=[self makeButton:CGRectMake((winWidth-175)/2, (winHeight-40)/2+50, 175, 40) title:NSLocalizedString(@"MOREGAMES", nil)];
     
     UIView *seperator4 = [[UIView alloc] initWithFrame:CGRectMake((winWidth-175)/2, (winHeight-40)/2+95, 175, 3.0)];
     [seperator4 setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"double_line.png"]]];
 
-    
+    [self.view addSubview:closeButton];
     [self.view addSubview:seperator1];
     [self.view addSubview:btnReset];
     [self.view addSubview:seperator2];
@@ -58,6 +78,7 @@
     [self.view addSubview:btnMoreGames];
     [self.view addSubview:seperator4];
     
+    [self setBackgrounds];
     
 }
 
@@ -71,23 +92,27 @@
 }
 
 -(UIButton *) makeButton:(CGRect)frame title:(NSString *) title{
-    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = frame;
-
-    btn.titleLabel.font=[UIFont fontWithName:@"Helvetica-Light" size:25.0];
-    btn.titleLabel.numberOfLines=2;
+    UIButton * btn=[UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setFrame:frame];
+    UILabel * lblReset=[[UILabel alloc]initWithFrame:CGRectMake(0, 0, buttonWidth, buttonHeight)];
+    UIFont * font=[UIFont fontWithName:@"Helvetica-Light" size:25.0];
     
-    [btn setTitleColor:[UIColor colorWithRed:0.462 green:0.364 blue:0.227 alpha:1.0] forState:UIControlStateNormal];
-    [btn setTitle:title forState:UIControlStateNormal];
-    [btn setTitleShadowColor:[UIColor colorWithWhite:1.0 alpha:0.7] forState:UIControlStateNormal];
-
-    btn.titleLabel.backgroundColor=[UIColor clearColor];
-    btn.titleLabel.textAlignment=NSTextAlignmentCenter;
+    [lblReset setText:title];
+    [lblReset setFont:font];
+    [lblReset setBackgroundColor:[UIColor clearColor]];
+    [lblReset setTextColor:[UIColor blackColor]];
+    [lblReset setShadowColor:[UIColor colorWithWhite:1.0 alpha:0.7]];
+    [lblReset setShadowOffset:CGSizeMake(0.0, 1.0)];
+    [lblReset setTextAlignment:NSTextAlignmentCenter];
+    [btn addSubview:lblReset];
     
     return btn;
     
 }
 
+- (void) closeStats {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 
 - (void)didReceiveMemoryWarning
 {
