56194570e1c567c934bd2346825fc8cb7b0c744e
edebimce <abdullah@halici.com.tr>
2013-03-21 16:00:53 +0200

    EQ - update About Us

--- Equify/Equify/Views/EQSettingsViewController.m
+++ Equify/Equify/Views/EQSettingsViewController.m
@@ -192,7 +192,7 @@
     UIColor *color = [UIColor colorWithRed:0.298 green:0.298 blue:0.298 alpha:1.0];
     
     // Company Name
-    UILabel * cName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 0, mask.frame.size.width, 40.0)];
+    UILabel * cName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 40, mask.frame.size.width, 22.0)];
     [cName setFont:[UIFont fontWithName:fontHeader size:fontSizeL]];
     [cName setTextColor:color];
     [cName setTextAlignment:NSTextAlignmentCenter];
@@ -200,7 +200,7 @@
     [cName setText:@"HALICI BİLGİ İŞLEM A.Ş."];
     
     // Adress
-    UILabel * cAdress=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 40, mask.frame.size.width, 120)];
+    UILabel * cAdress=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 50, mask.frame.size.width, 120)];
     [cAdress setFont:[UIFont fontWithName:font size:fontSizeM]];
     [cAdress setTextColor:color];
     [cAdress setTextAlignment:NSTextAlignmentCenter];
@@ -209,7 +209,7 @@
     [cAdress setText:@"ODTÜ-Halıcı Yazılımevi \nİnönü Bulvarı 06531 \nODTÜ-Teknokent/ANKARA"];
     
     // Mail
-    UILabel * cMail=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 160, mask.frame.size.width, 40.0)];
+    UILabel * cMail=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 150, mask.frame.size.width, 40.0)];
     [cMail setFont:[UIFont fontWithName:font size:fontSizeM]];
     [cMail setTextColor:color];
     [cMail setTextAlignment:NSTextAlignmentCenter];
@@ -218,7 +218,7 @@
     
     
     // Programming
-    UILabel * cProgramming=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 240, mask.frame.size.width, 40.0)];
+    UILabel * cProgramming=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 200, mask.frame.size.width, 40.0)];
     [cProgramming setFont:[UIFont fontWithName:fontHeader size:fontSizeL]];
     [cProgramming setTextColor:color];
     [cProgramming setTextAlignment:NSTextAlignmentCenter];
@@ -229,7 +229,7 @@
     // Names
     NSArray * names=[[NSArray alloc] initWithObjects:@"Eren HALICI",@"Yunus Eren GÜZEL", @"Abdullah KARACABEY",@"Alperen KAVUN", nil];
     for(int i=0; i<names.count;i++){
-        UILabel * cName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 300+i*40, mask.frame.size.width, 40.0)];
+        UILabel * cName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 230+i*30, mask.frame.size.width, 40.0)];
         [cName setFont:[UIFont fontWithName:font size:fontSizeM]];
         [cName setTextColor:color];
         [cName setTextAlignment:NSTextAlignmentCenter];
@@ -241,14 +241,14 @@
     
     
     // Art
-    UILabel * cArt=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 500, mask.frame.size.width, 40.0)];
+    UILabel * cArt=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 370, mask.frame.size.width, 40.0)];
     [cArt setFont:[UIFont fontWithName:fontHeader size:fontSizeL]];
     [cArt setTextColor:color];
     [cArt setTextAlignment:NSTextAlignmentCenter];
     [cArt setBackgroundColor:[UIColor clearColor]];
     [cArt setText:NSLocalizedString(@"ART", nil)];
     
-    UILabel * cArtName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 540, mask.frame.size.width, 40.0)];
+    UILabel * cArtName=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 400, mask.frame.size.width, 40.0)];
     [cArtName setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:fontSizeM]];
     [cArtName setTextColor:color];
     [cArtName setTextAlignment:NSTextAlignmentCenter];
@@ -258,12 +258,21 @@
     
     
     // Copyright
-    UILabel * cCRight=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 620, mask.frame.size.width, 40.0)];
+    UILabel * cCRight=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 600, mask.frame.size.width, 40.0)];
     [cCRight setFont:[UIFont fontWithName:font size:fontSizeL]];
     [cCRight setTextColor:color];
     [cCRight setTextAlignment:NSTextAlignmentCenter];
     [cCRight setBackgroundColor:[UIColor clearColor]];
     [cCRight setText:@"Copyright © 2013"];
+    
+    // BrainQuire
+    UILabel * cBrainQuire=[[UILabel alloc] initWithFrame:CGRectMake(0.0, 630, mask.frame.size.width, 40.0)];
+    [cBrainQuire setFont:[UIFont fontWithName:font size:fontSizeL]];
+    [cBrainQuire setTextColor:color];
+    [cBrainQuire setTextAlignment:NSTextAlignmentCenter];
+    [cBrainQuire setBackgroundColor:[UIColor clearColor]];
+    [cBrainQuire setText:@"www.brainquire.com"];
+
     
     
     
@@ -274,6 +283,7 @@
     [credits addSubview:cArt];
     [credits addSubview:cArtName];
     [credits addSubview:cCRight];
+    [credits addSubview:cBrainQuire];
     [mask addSubview:credits];
     [aboutScreen addSubview:headerLabel];
     [aboutScreen addSubview:headerDoubleLine];
