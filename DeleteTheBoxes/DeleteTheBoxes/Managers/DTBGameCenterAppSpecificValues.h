30c81649011aa5f1e704e3f200bed6e2342da058
Eren Halici <eren@halici.com.tr>
2013-03-26 17:01:48 +0200

    Removed applications from oyun

--- DeleteTheBoxes/DeleteTheBoxes/Managers/DTBGameCenterAppSpecificValues.h
+++
@@ -1,9 +0,0 @@
-//
-//  DTBAppSpecificValues.h
-//  DeleteTheBoxes
-//
-//  Created by Alperen Kavun on 11.02.2013.
-//  Copyright (c) 2013 Halıcı. All rights reserved.
-//
-
-#define kAuthenticationChangedNotification @"AuthenticationChangedNotification"
